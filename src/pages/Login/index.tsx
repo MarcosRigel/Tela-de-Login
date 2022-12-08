d7aaf20b12acfbbf4e4e0469d1191fa445bc429e
Marcos Rigel <marcosrigel99@gmail.com>
2022-12-08 14:49:25 -0300

    projeto iniciado

---
+++ src/pages/Login/index.tsx
@@ -0,0 +1,16 @@
+import Button from "../ ../components/Button";
+import Input from "../ ../components/Input";
+
+import { Container, LoginContainer, Column, Spacing, Title }
+
+const Login = () => {
+    return (
+        <Container>
+        <LoginContainer>
+
+        </LoginContainer>
+        <Container/>
+    );
+};
+
+export default Login;
